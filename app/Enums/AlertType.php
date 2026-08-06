00583d6fb517cef62e638bc623f46696555f00a7
Andrés Cebay <pintech.sistemas@gmail.com>
2026-08-06 12:33:32 -0500

    feat(paint-development-requests): add alert integration on submit

--- app/Enums/AlertType.php
+++ app/Enums/AlertType.php
@@ -7,6 +7,7 @@ enum AlertType: string
     case StockBajo = 'stock_bajo';
     case VencimientoProximo = 'vencimiento_proximo';
     case VariacionPrecio = 'variacion_precio';
+    case PaintDevelopmentRequest = 'paint_development_request';
 
     public function label(): string
     {
@@ -14,6 +15,7 @@ enum AlertType: string
             self::StockBajo => __('Stock bajo'),
             self::VencimientoProximo => __('Vencimiento próximo'),
             self::VariacionPrecio => __('Variación de precio'),
+            self::PaintDevelopmentRequest => __('Solicitud de desarrollo de pintura'),
         };
     }
 }
